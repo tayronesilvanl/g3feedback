ad6f1e64901626e01508c463dbe18d1d9d4acb46
Tayrone da Silva <121695470+tayronesilvanl@users.noreply.github.com>
2023-10-25 13:24:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -63,5 +63,4 @@ def adicionar_dados_excel(data):
 
 # Ponto de entrada principal
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
-
+    app.run(debug=True)
